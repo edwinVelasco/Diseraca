37e6937134b82bd502c0f94ef2b59ca21115bfa5
Edwin Velasco <edwinalbertocelasco8@gmail.com>
2018-03-02 21:24:18 -0500

    actualizacion de registro masivo de asistencias

--- Diseraca/models.py
+++ Diseraca/models.py
@@ -229,8 +229,8 @@ class Beca_Turno(models.Model):
     status = models.BooleanField(default=True)
 
     def __unicode__(self):
-        return "{0}-{1}".format(str(self.turno).encode('utf-8'),
-                                self.beca.nick.encode('utf-8'))
+        salida = u'{0}-{1}'.format(self.turno, self.beca.nick)
+        return salida.encode('utf-8')
 
     class Meta:
         unique_together = (('beca', 'turno'),)
