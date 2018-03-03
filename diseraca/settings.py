43f99de9bd7d562dde3b572ade13366ffccb6801
Edwin Velasco <edwinalbertocelasco8@gmail.com>
2018-03-02 20:33:52 -0500

    settings

--- diseraca/settings.py
+++ diseraca/settings.py
@@ -82,6 +82,18 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'diseraca',
+        'USER': 'wolf',
+        'PASSWORD': 'Claro"##0',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+'''
 
 
 # Internationalization
